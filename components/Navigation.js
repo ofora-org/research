fb2418fd651bb469904cc7a03e763aa74cb07512
Diogo Vianna V. Araújo <diogovaraujo@gmail.com>
2018-05-19 18:54:50 -0300

    Updated arrow position on first slide

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,7 +2,15 @@ import React from 'react'
 import { withState, withHandlers, lifecycle, compose } from 'recompose'
 
 const enhance = compose(
-  withState('currentScreen', 'setScreen', 0)
+  withState('currentScreen', 'setScreen', 0),
+  // withHandlers({
+  //   onClickLeft: ({ children, setScreen, currentScreen }) => e => {
+  //     currentScreen === children.length ?
+  //   },
+  //   onClickRight: props => e => {
+  //
+  //   }
+  // })
 )
 
 const Navigation = ({children, currentScreen, setScreen}) =>
@@ -11,8 +19,12 @@ const Navigation = ({children, currentScreen, setScreen}) =>
       {children}
     </div>
     <div className='control'>
-      <span className='left' onClick={() => setScreen(0)}>←</span>
-      <span className='right' onClick={() => setScreen(1)}>→</span>
+      {currentScreen !== 0 ?
+        <span className='left' onClick={() => setScreen(currentScreen-1)}>←</span>
+      : null}
+      {currentScreen !== children.length-1 ?
+        <span className='right' onClick={() => setScreen(currentScreen+1)}>→</span>
+      : null}
     </div>
     <style jsx>{`
       .wrapper {
@@ -23,15 +35,20 @@ const Navigation = ({children, currentScreen, setScreen}) =>
       .navigationWrapper {
         display: flex;
         height: 100%;
-        ransition: .6s transform;
+        transition: .6s transform;
         transform: translateX(-${currentScreen*100}%)
       }
       .control {
         font-family: arial;
         position: fixed;
-        right: 20px;
+        right: ${currentScreen === 0 ? '50%' : '20px'};
+        transform: translateX(${currentScreen === 0 ? '50%' : '0'});
         bottom: 20px;
         font-size: 80px;
+        cursor: pointer;
+      }
+      .control > span {
+        padding: 0 10px
       }
     `}</style>
   </div>
