26e9fe799d4aabd25be2a77514805582634765b9
Diogo Vianna <diogoviannaaraujo@Diogos-MBP.home>
2018-07-21 16:26:44 -0300

    Added cep search

--- components/CEPInput.js
+++ components/CEPInput.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { withState, withHandlers, compose } from 'recompose'
 import InputMask from 'react-input-mask';
+import fetch from 'isomorphic-unfetch'
 
 const CEPInput = props =>
   <div className='root'>
@@ -13,9 +14,9 @@ const CEPInput = props =>
         <div className='search-cep' onClick={props.handleSearchCepShow} />
         <div className='inner'>
           <h3>Digite a baixo bla bla bla...</h3>
-          <input type='text' placeholder='Estado (UF)' size={9} />
-          <input type='text' placeholder='Cidade' size={9} />
-          <input type='text' placeholder='Rua' />
+          <input type='text' placeholder='Estado (UF)' size={9} maxLength={2} className='uf' />
+          <input type='text' placeholder='Cidade' size={9} className='city' />
+          <input type='text' placeholder='Rua' className='street' />
           <div><button onClick={props.handleSearchCepSubmit}>Buscar</button></div>
         </div>
       </div>
@@ -52,8 +53,18 @@ const CEPInput = props =>
 export default compose(
   withState('isSearchCepVisible', 'setSearchCepVisible', false),
   withHandlers({
-    handleSearchCepSubmit: ({handleSearchCepSubmit}) => e => {
-      return alert('oi')
+    handleSearchCepSubmit: ({
+        setSearchCepVisible,
+        onChange
+      }) => async e => {
+      const uf = document.querySelector('.search-cep input.uf').value
+      const city = document.querySelector('.search-cep input.city').value
+      const street = document.querySelector('.search-cep input.street').value
+      const response = await fetch(`https://viacep.com.br/ws/${uf}/${city}/${street}/json/`)
+      const json = await response.json()
+      if (json.length === 0) return alert('O endereço não foi encontrado!')
+      onChange({target: { value: json[0].cep}})
+      setSearchCepVisible(false)
     },
     handleSearchCepShow: ({setSearchCepVisible, isSearchCepVisible}) => e => {
       setSearchCepVisible(!isSearchCepVisible)
