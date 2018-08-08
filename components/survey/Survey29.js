b314c8d17e189503492d1db50c8fd4d0b62d726f
Diogo Vianna <diogovaraujo@gmail.com>
2018-08-08 00:50:57 -0300

    typo

--- components/survey/Survey29.js
+++ components/survey/Survey29.js
@@ -13,7 +13,7 @@ const Content = ({value, onChange, onButtonClick}) =>
   <div className='content'>
     <div className='background' />
     <Title><span className='title'>Acabou, valeu, obrigado!</span></Title>
-    <p>Para não ficar de Fora e receber os resultados da pesquisa, notícias sobre atividades e convites para participar, deixe seu e-mail:<p>
+    <p>Para não ficar de Fora e receber os resultados da pesquisa, notícias sobre atividades e convites para participar, deixe seu e-mail:</p>
     <input type='text' placeholder='Digite aqui' onChange={e => onChange(29, e.target.value)} />
     <div><a onClick={onButtonClick}>Enviar</a></div>
 <div><a href="https://ofora.org/">site</a>, <a href="https://facebook.com/">Facebook</a> ou <a href="https://instagram.com/">Instagram.</a></div>
