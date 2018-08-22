109253339b678935806494cdaa1d6bec1208080f
Diogo Vianna Valadares Araújo <diogoviannaaraujo@Diogos-MBP.home>
2018-08-22 20:00:31 -0300

    Images missiong on mobile

--- components/survey/Survey15.js
+++ components/survey/Survey15.js
@@ -45,7 +45,6 @@ const Content = ({value, onChange}) =>
 const Image = () =>
   <div className='image'>
     <style jsx>{`
-      @media only screen and (min-width: 720px) {
         .image {
           position: absolute;
           top: 0; left: 0; right: 0; bottom: 0;
@@ -53,7 +52,6 @@ const Image = () =>
           background-size: cover;
           background-position: bottom center;
         }
-      }
     `}</style>
   </div>
 
