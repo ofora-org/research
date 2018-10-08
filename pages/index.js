2c8a17abe57e23fb4b7b16b1845b50c20e577c5e
Diogo Vianna <diogovaraujo@gmail.com>
2018-10-07 22:26:20 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ import Survey4 from 'components/survey/Survey4'
 import Survey5 from 'components/survey/Survey5'
 import Survey6 from 'components/survey/Survey6'
 import Survey7 from 'components/survey/Survey7'
+import Survey8 from 'components/survey/Survey8'
 import Survey9 from 'components/survey/Survey9'
 import Survey10 from 'components/survey/Survey10'
 import Survey11 from 'components/survey/Survey11'
@@ -25,14 +26,13 @@ import Survey18 from 'components/survey/Survey18'
 import Survey19 from 'components/survey/Survey19'
 import Survey20 from 'components/survey/Survey20'
 import Survey21 from 'components/survey/Survey21'
-import Survey22 from 'components/survey/Survey22'
-import Survey23 from 'components/survey/Survey23'
-import Survey24 from 'components/survey/Survey24'
-import Survey25 from 'components/survey/Survey25'
-import Survey26 from 'components/survey/Survey26'
-import Survey27 from 'components/survey/Survey27'
-import Survey28 from 'components/survey/Survey28'
-import Survey29 from 'components/survey/Survey29'
+import Survey23 from 'components/survey/Survey23';
+import Survey24 from 'components/survey/Survey24';
+import Survey25 from 'components/survey/Survey25';
+import Survey26 from 'components/survey/Survey26';
+import Survey27 from 'components/survey/Survey27';
+import Survey28 from 'components/survey/Survey28';
+import Survey29 from 'components/survey/Survey29';
 
 const Index = ({onChangeHandler, onNavigateHandler, value, setValue}) =>
   <div className='page-wrapper'>
@@ -49,22 +49,20 @@ const Index = ({onChangeHandler, onNavigateHandler, value, setValue}) =>
         <Survey5 onChange={onChangeHandler} key={5} value={value['5'] ? value['5'] : {}} />,
         <Survey6 onChange={onChangeHandler} key={6} value={value['6']} />,
         <Survey7 onChange={onChangeHandler} key={7} value={value['7']} />,
+        <Survey8 onChange={onChangeHandler} key={8} value={value['8']} />,
         <Survey9 onChange={onChangeHandler} key={9} value={value['9']} />,
         <Survey10 onChange={onChangeHandler} key={10} value={value['10']} />,
         <Survey11 onChange={onChangeHandler} key={11} value={value} />,
+        ...(value[10] && !value[10].includes('Moro') ? [<Survey17 onChange={onChangeHandler} key={17} value={value['17']} />] : []),
+        ...(value[10] && !value[10].includes('Moro') ? [<Survey18 onChange={onChangeHandler} key={18} value={value['18']} />] : []),
         <Survey12 onChange={onChangeHandler} key={12} value={value} />,
         <Survey13 onChange={onChangeHandler} key={13} value={value['13']} />,
         <Survey14 onChange={onChangeHandler} key={14} value={value['14']} />,
         <Survey15 onChange={onChangeHandler} key={15} value={value['15']} />,
         ...(value[14] && (value[14] !== '5 a 7 vezes por semana') ? [<Survey16 onChange={onChangeHandler} key={16} value={value['16']} />] : []),
         <Survey19 onChange={onChangeHandler} key={19} value={value['19']} />,
-        <Survey20 onChange={onChangeHandler} key={20} value={value['20']} />,
+        <Survey20 onChange={onChangeHandler} key={20} value={value['20'] ? value['20'} : {} />,
         <Survey21 onChange={onChangeHandler} key={21} value={value['21']} />,
-        <Survey22 onChange={onChangeHandler} key={22} value={value['22']} />,
-        <Survey23 onChange={onChangeHandler} key={23} value={value['23']} />,
-        <Survey24 onChange={onChangeHandler} key={24} value={value['24']} />,
-        <Survey25 onChange={onChangeHandler} key={25} value={value['25']} />,
-        <Survey26 onChange={onChangeHandler} key={26} value={value['26']} />,
         <Survey27 onChange={onChangeHandler} key={27} value={value['27']} />,
         <Survey28 onChange={onChangeHandler} key={28} value={value['28']} />,
         <Survey29 onChange={onChangeHandler} key={29} value={value['29']} onButtonClick={()=>onNavigateHandler(29,29)}/>
