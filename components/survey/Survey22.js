cde6452d0555cb7a388e89d4a20fbe22557d7ed3
Diogo Vianna <diogovaraujo@gmail.com>
2018-10-16 23:18:58 -0300

    Create Survey22.js

---
+++ components/survey/Survey22.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import ContentWrapper from 'components/ContentWrapper'
+import Title from 'components/Title'
+import Subtitle from 'components/Subtitle'
+import AsteriscNote from '../AsteriscNote';
+import SingleChoice from 'components/Choice/SingleChoice'
+
+const Survey11 = (props) =>
+  <ContentWrapper
+    left={<Content {...props} />}
+  />
+
+const Content = ({value, onChange}) =>
+  <div className='content'>
+    <Title><span className='title'>Você tem espaço(s) público(s) de convívio* favorito(s) em São Paulo?</span></Title>
+    <Subtitle>Em caso de ter, indique quais na caixa de texto.</Subtitle>
+    <SingleChoice
+      choices={['Não']}
+      onChange={itemValue => onChange(22, itemValue)}
+      value={value && value[1]}
+      other='Sim, quais?'
+      otherPlaceholder='Cite aqui quais são, é importante!'
+    />
+    <AsteriscNote>* Ruas, praças, parques, quadras, ou outras áreas abertas para aproveitar a cidade.</AsteriscNote>
+    <style jsx>{`
+      .content {
+        display: flex;
+        flex-direction: column;
+        max-height: 100%;
+      }
+      .title {
+        padding-right: 90px;
+        display: block;
+        font-size: 0.8em
+      }
+      .formGroup {
+        margin-bottom: 1em;
+      }
+      label {
+        display: block;
+        font-weight: bold;
+        font-size: 1.8em;
+        color: #0009FF;
+      }
+      label span {
+        -webkit-text-stroke: 1px blue;
+        color: transparent;
+        font-style: italic;
+      }
+      @media only screen and (min-width: 720px) {
+        .title {
+          padding-right: 140px;
+        }
+      }
+    `}</style>
+  </div>
+
+export default Survey11
